b2fb3982364bafe5f15545b0c1e6c3182c9ee4e1
Taylor123 <nftaylor11@gmail.com>
2024-11-21 17:14:45 -0500

    fix: client must generate deposit_receipt_base and pass the pubkey

--- api/src/router.rs
+++ api/src/router.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use axum::{http::StatusCode, routing::get, Router};
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
 
-mod get_deposit_stake_instruction;
+mod get_deposit_stake_instructions;
 
 pub struct RouterState {
     pub rpc_client: RpcClient,
@@ -12,7 +12,7 @@ pub struct RouterState {
 pub fn get_routes(state: Arc<RouterState>) -> Router {
     let api_routes = Router::new().route(
         "/get-deposit-stake-instructions",
-        get(get_deposit_stake_instruction::get_deposit_stake_instruction),
+        get(get_deposit_stake_instructions::get_deposit_stake_instructions),
     );
 
     let app = Router::new().nest("/api/v1", api_routes).fallback(fallback);
